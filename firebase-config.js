200fbd9dcb50410e659c49002e0599b6a09afea0
Guilherme Pereira Miranda <224416698+guilhermepmdev@users.noreply.github.com>
2026-07-31 08:52:01 -0300

    Revise Firebase config and include initialization script
    Updated Firebase configuration and added script imports.

--- firebase-config.js
+++ firebase-config.js
@@ -9,12 +9,26 @@
    pelos domínios autorizados em Authentication > Settings.
    ========================================================================== */
 
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyABd-M3KMhyZ-pX5Q1MVgqrVwCjGWknWwM",
-  authDomain: "edusense-7b027.firebaseapp.com",
-  projectId: "edusense-7b027",
-  storageBucket: "edusense-7b027.firebasestorage.app",
-  messagingSenderId: "799185145852",
-  appId: "1:799185145852:web:0c1c98acbacb29d916ec79",
-  measurementId: "G-RLGWGC2WML"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyA8lCSb3QWRoYAZQHavcJo3YzP7dTA9vN8",
+    authDomain: "edusense-7b027.firebaseapp.com",
+    projectId: "edusense-7b027",
+    storageBucket: "edusense-7b027.firebasestorage.app",
+    messagingSenderId: "799185145852",
+    appId: "1:799185145852:web:0df7faf3889e559916ec79",
+    measurementId: "G-ZPFDGLRE1S"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
