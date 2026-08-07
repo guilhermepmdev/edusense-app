b9ea996e56f853c4b9672f4b6e1f157b9afc774c
kauapaulareis-ctrl <kaua.paula.reis@escola.pr.gov.br>
2026-08-07 08:19:09 -0300

    Refactor firebase config variable name to uppercase

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,4 @@
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-analytics.js";
-
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
-
-
-const firebaseConfig = {
+const FIREBASE_CONFIG = {
   apiKey: "AIzaSyABd-M3KMhyZ-pX5Q1MVgqrVwCjGWknWwM",
   authDomain: "edusense-7b027.firebaseapp.com",
   projectId: "edusense-7b027",
@@ -15,10 +7,3 @@ const firebaseConfig = {
   appId: "1:799185145852:web:0c1c98acbacb29d916ec79",
   measurementId: "G-RLGWGC2WML"
 };
-
-
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-
-
-export const auth = getAuth(app);
